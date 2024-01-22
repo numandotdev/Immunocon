87d3c33e4367b34efd6467717ab49cb691b7cb74
numandotdev <numandotdev@gmail.com>
2024-01-22 18:05:58 +0530

    Added logo slider

---
+++ scripts/logo.slider.js
@@ -0,0 +1,5 @@
+const logoSlides = document.querySelector('.logo_slides');
+const DupliLogoSlides = logoSlides.cloneNode(true);
+logoSlides.parentNode.appendChild(DupliLogoSlides)
+// const DupliLogoSlides1 = logoSlides.cloneNode(true);
+// logoSlides.parentNode.appendChild(DupliLogoSlides1)
